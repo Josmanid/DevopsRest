60f455d35994a8048f747b4e2dacbef07fd6124f
Jonas Martin Smidt <jos005@edu.zealand.dk>
2025-04-25 14:07:10 +0200

    Added Test

---
+++ DevopsRestTests/RepositoryList/EggsRepositoryTests.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using DevopsRest.RepositoryList;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DevopsRest.RepositoryList.Tests
+{
+    [TestClass()]
+    public class EggsRepositoryTests
+    {
+        [TestMethod()]
+        public void GetByIdTest() {
+            Assert.Fail();
+        }
+    }
+}
