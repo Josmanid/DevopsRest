18df453bd570269aea7246ad4b96d52b5bb36620
Jonas Martin Smidt <jos005@edu.zealand.dk>
2025-04-25 14:27:04 +0200

    Get Egg by Id Test

--- DevopsRestTests/RepositoryList/EggsRepositoryTests.cs
+++ DevopsRestTests/RepositoryList/EggsRepositoryTests.cs
@@ -5,15 +5,32 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using DevopsRest.Models;
+using System.Reflection;
 
 namespace DevopsRest.RepositoryList.Tests
 {
     [TestClass()]
     public class EggsRepositoryTests
     {
+
+        private EggsRepository _repository;
+        [TestInitialize]
+        public void setup() {
+            //Arrange
+            _repository = new EggsRepository();
+        }
         [TestMethod()]
-        public void GetByIdTest() {
-            Assert.Fail();
+        public void GetEggByIdTest() {
+            // Act i already have som eggs to look at
+            Egg PositiveEgg = _repository.GetById(1);
+            Egg NegativeEgg = _repository.GetById(999);
+
+            // Assert
+            Assert.AreEqual(1,PositiveEgg.Id);
+            Assert.AreEqual("Arla",PositiveEgg.Name);
+            Assert.IsNull(NegativeEgg);
+
         }
     }
 }
